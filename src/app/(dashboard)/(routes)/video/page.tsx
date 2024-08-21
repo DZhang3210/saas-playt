faff1bf6ff7e856cd4ee69948bc174dbdd5d1753
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-08-21 14:58:42 -0400

    new commit

---
+++ src/app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import Heading from "@/components/heading";
+import * as z from "zod";
+import { Music, VideoIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Empty from "@/components/empty";
+import { Loader } from "@/components/loader";
+
+// Define message type (ensure it matches the type returned by OpenAI)
+type Message = {
+  role: string;
+  content: string;
+};
+
+export default function VideoPage() {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+      console.log(values);
+      const response = await axios.post("/api/video", {
+        prompt: values.prompt,
+      });
+      console.log("RESPONSE", response);
+
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error: any) {
+      // TODO: Handle errors appropriately
+      console.error(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompt into Video"
+        icon={VideoIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid w-full grid-cols-12 rounded-lg border p-4 px-3 focus-within:shadow-sm md:px-6"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="focus-visible: border-0 outline-none ring-transparent focus-visible:ring-0"
+                        disabled={isLoading}
+                        placeholder="Clown fish swimming around a coral reef"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="col-span-12 w-full lg:col-span-2"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="flex w-full items-center justify-center rounded-lg bg-muted p-8">
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && (
+            <div>
+              <Empty label="No video generated." />
+            </div>
+          )}
+          {video && (
+            <video
+              controls
+              className="mt-8 aspect-video w-full rounded-lg border bg-black"
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
